26d00806fbd8dc9463126be539621f6e88c60f9f
Matt Buck <matt@voxable.io>
2022-09-08 07:24:27 -0500

    Add ui library

---
+++ libs/ui/src/lib/ui.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+/* eslint-disable-next-line */
+export interface UiProps {}
+
+const StyledUi = styled.div`
+  color: pink;
+`;
+
+export function Ui(props: UiProps) {
+  return (
+    <StyledUi>
+      <h1>Welcome to Ui!</h1>
+    </StyledUi>
+  );
+}
+
+export default Ui;
