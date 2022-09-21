f65b434389f4d85331488fd655019ca124e4f926
Matt Buck <m@chaosre.actor>
2022-09-21 02:10:03 +0000

    Fix typos

--- apps/chaosreactor/jest.config.ts
+++ apps/chaosreactor/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'chaosreactoractor',
+  displayName: 'chaosreactor',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
