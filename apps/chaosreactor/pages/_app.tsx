b75782a3eec17f0223ca3771696ef283b4dca19b
Matt Buck <matt@voxable.io>
2022-09-28 11:59:19 -0400

    Switch to Chakra Pro and Work Sans

--- apps/chaosreactor/pages/_app.tsx
+++ apps/chaosreactor/pages/_app.tsx
@@ -2,11 +2,24 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
 // pages/_app.js
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { theme } from '@chakra-ui/pro-theme';
+import '@fontsource/work-sans';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const myTheme = extendTheme(
+    {
+      colors: { ...theme.colors, brand: theme.colors.purple },
+      fonts: {
+        heading: 'Work Sans, -apple-system, system-ui, sans-serif',
+        body: 'Work Sans, -apple-system, system-ui, sans-serif',
+      },
+    },
+    theme
+  );
+
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={myTheme}>
       <Head>
         <title>Welcome to chaosreactor!</title>
       </Head>
