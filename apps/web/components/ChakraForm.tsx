08578b0d62d0e197ffadb1c544f58e139df2d540
Matt Buck <matt@voxable.io>
2022-10-04 01:13:23 -0400

    Add a Chakra signup form

--- apps/web/components/ChakraForm.tsx
+++ apps/web/components/ChakraForm.tsx
@@ -13,36 +13,91 @@ import {
   Text,
   useBreakpointValue,
 } from '@chakra-ui/react';
-import * as React from 'react';
+import { useEffect, useState } from 'react';
+import useAxios from 'axios-hooks';
 
-const ChakraForm = () => (
-  <ChakraProvider theme={chaosTheme}>
-    <Container minW="max-content" px="0" py={{ base: '0' }}>
-      <Stack spacing="5">
-        <FormControl>
-          <FormLabel color="white" htmlFor="name">
-            First name
-          </FormLabel>
-          <Input id="first_name" type="text" />
-        </FormControl>
-        <FormControl>
-          <FormLabel color="white" htmlFor="name">
-            Last name
-          </FormLabel>
-          <Input id="last_name" type="text" />
-        </FormControl>
-        <FormControl isRequired>
-          <FormLabel color="white" htmlFor="email">
-            Email
-          </FormLabel>
-          <Input id="email" type="email" />
-        </FormControl>
-        <Button fontWeight="bold" textTransform="capitalize" variant="primary">
-          Sign up
-        </Button>
-      </Stack>
-    </Container>
-  </ChakraProvider>
-);
+const ChakraForm = () => {
+  const [email, setEmail] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [pageUri, setPageUri] = useState<string>();
+
+  const [{ data, loading, error }, refetch] = useAxios(
+    {
+      url: '/api/emailSignup',
+      method: 'POST',
+      data: { email, pageUri },
+    },
+    {
+      manual: true,
+    }
+  );
+
+  // Clear the form on successful submitting it
+  useEffect(() => {
+    if (data?.success === true && !loading) {
+      setEmail('');
+    }
+  }, [data?.success, loading]);
+
+  // Get the url the user is currently visiting.
+  // Optional, but enriches the data we have in HubSpot.
+  useEffect(() => {
+    setPageUri(window.location.href);
+  });
+
+  return (
+    <ChakraProvider theme={chaosTheme}>
+      <Container minW="max-content" px="0" py={{ base: '0' }}>
+        <Stack spacing="8">
+          <FormControl>
+            <FormLabel color="white" htmlFor="firstName">
+              First name
+            </FormLabel>
+            <Input
+              id="firstName"
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel color="white" htmlFor="lastName">
+              Last name
+            </FormLabel>
+            <Input
+              id="lastName"
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel color="white" htmlFor="email">
+              Email
+            </FormLabel>
+            <Input
+              id="email"
+              type="email"
+              value={email}
+              placeholder="me@example.com"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FormControl>
+          <Button
+            fontWeight="bold"
+            textTransform="capitalize"
+            variant="primary"
+            type={'submit'}
+            onClick={() => refetch()}
+            disabled={loading}
+          >
+            Sign up
+          </Button>
+        </Stack>
+      </Container>
+    </ChakraProvider>
+  );
+};
 
 export default ChakraForm;
