d924cd1db2573a302017ddff4ed660d739ee7603
Matt Buck <matt@voxable.io>
2022-11-10 13:11:45 -0500

    Fix broken links in docs

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro"
+            to="/docs/Overview"
           >
             Create AI generated content
           </Link>
