fac67aee66323a454ecaf937a0d9a30871ead2f9
Matt Buck <matt@voxable.io>
2022-11-11 12:58:25 -0500

    Enable launching dolt server on desktop launch

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -1,4 +1,6 @@
-#![cfg_attr(
+use tauri::api::process::Command;
+
+#[cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
@@ -9,7 +11,21 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+fn launch_dolt() -> Result<(), String> {
+  println!("Launching Dolt");
+
+  Command::new_sidecar("dolt")
+    .or(Err(String::from("Can't find dolt binary")))?
+    .args( &["sql-server"])
+    .spawn()
+    .map_err(|err| format!("Error launching dolt {:?}", err))?;
+
+  Ok(())
+}
+
 fn main() {
+    launch_dolt().unwrap();
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
