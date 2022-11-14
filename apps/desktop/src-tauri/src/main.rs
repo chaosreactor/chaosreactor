ea096813f3bbb0561af0111dd9aeace95fe7f555
Matt Buck <matt@voxable.io>
2022-11-14 13:10:40 -0500

    Fix issues with Rust editor setup

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -1,10 +1,11 @@
+use std::path::Path;
+use tauri::api::path::data_dir;
 use tauri::api::process::Command;
 
 #[cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -12,15 +13,26 @@ fn greet(name: &str) -> String {
 }
 
 fn launch_dolt() -> Result<(), String> {
-  println!("Launching Dolt");
+    println!("Launching Dolt");
 
-  Command::new_sidecar("dolt")
-    .or(Err(String::from("Can't find dolt binary")))?
-    .args( &["sql-server"])
-    .spawn()
-    .map_err(|err| format!("Error launching dolt {:?}", err))?;
+    // Build the option to use the data directory as the dolt directory.
+    let dolt_data_path = "--data-dir ".to_owned()
+        + &data_dir()
+            .unwrap()
+            .join("dolt")
+            .into_os_string()
+            .into_string()
+            .unwrap();
 
-  Ok(())
+    println!("Dolt dir: {}", dolt_dir);
+
+    Command::new_sidecar("dolt")
+        .or(Err(String::from("Can't find dolt binary")))?
+        .args(&["sql-server", &dolt_dir])
+        .spawn()
+        .map_err(|err| format!("Error launching dolt {:?}", err))?;
+
+    Ok(())
 }
 
 fn main() {
