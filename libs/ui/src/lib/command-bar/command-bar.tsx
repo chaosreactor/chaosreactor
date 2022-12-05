71876d0d1088fc9ef4031de23ab9931b1e2ad26e
Matt Buck <matt@voxable.io>
2022-12-05 12:05:53 -0500

    Enable keyboard shortcut disclosure for CommandBar

--- libs/ui/src/lib/command-bar/command-bar.tsx
+++ libs/ui/src/lib/command-bar/command-bar.tsx
@@ -1,5 +1,5 @@
 import { Command } from 'cmdk';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Icon } from '@iconify/react';
 
 import { ImageGeneratorBlock } from '../playfield/blocks/image-generator';
@@ -22,65 +22,81 @@ export interface CommandBarProps {}
 
 export function CommandBar(props: CommandBarProps) {
   const [value, setValue] = useState('button');
+  const [open, setOpen] = useState(false);
 
   const onValueChange = (value: string) => {
     setValue(value);
   };
 
+  // Toggle the menu when ⌘K is pressed
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === 'k' && e.metaKey) {
+        if ((e.target as HTMLElement).tagName === 'BODY')
+          setOpen((open) => !open);
+      }
+    };
+
+    document.body.addEventListener('keydown', down);
+    return () => document.body.removeEventListener('keydown', down);
+  }, [open]);
+
   return (
-    <div className={styles['framer']}>
-      <Command value={value} onValueChange={onValueChange}>
-        <div cmdk-framer-header="">
-          <SearchIcon />
-          <Command.Input autoFocus placeholder="Search for blocks" />
-        </div>
-        <Command.List>
-          <div cmdk-framer-items="">
-            <div cmdk-framer-left="">
-              <Command.Group heading="Generators">
-                <Item
-                  value="Generate image"
-                  subtitle="Text description to image"
-                >
-                  <Icon icon="noto:framed-picture" />
-                </Item>
-                <Item value="Generate text" subtitle="Expand a prompt">
-                  <Icon icon="noto:memo" />
-                </Item>
-              </Command.Group>
-              <Command.Group heading="Operations">
-                <Item
-                  value="Classify"
-                  subtitle="Group, identify, or categorize"
-                >
-                  <Icon icon="noto:bullseye" />
-                </Item>
-              </Command.Group>
-              <Command.Group heading="Source data">
-                <Item value="Images" subtitle="Use existing images">
-                  <Icon icon="noto:framed-picture" />
-                </Item>
-                <Item value="Text" subtitle="Enter text or select files">
-                  <Icon icon="noto:memo" />
-                </Item>
-                <Item value="Movies" subtitle="Select movie files or URLs">
-                  <Icon icon="noto:movie-camera" />
-                </Item>
-              </Command.Group>
-            </div>
-            <hr cmdk-framer-separator="" />
-            <div cmdk-framer-right="">
-              {value === 'generate image' && <ImageGeneratorBlock />}
-              {value === 'input' && <Input />}
-              {value === 'badge' && <Badge />}
-              {value === 'radio' && <Radio />}
-              {value === 'slider' && <Slider />}
-              {value === 'container' && <Container />}
-            </div>
+    <Command.Dialog open={open} onOpenChange={setOpen}>
+      <div className={styles['framer']}>
+        <Command value={value} onValueChange={onValueChange}>
+          <div cmdk-framer-header="">
+            <SearchIcon />
+            <Command.Input autoFocus placeholder="Search for blocks" />
           </div>
-        </Command.List>
-      </Command>
-    </div>
+          <Command.List>
+            <div cmdk-framer-items="">
+              <div cmdk-framer-left="">
+                <Command.Group heading="Generators">
+                  <Item
+                    value="Generate image"
+                    subtitle="Text description to image"
+                  >
+                    <Icon icon="noto:framed-picture" />
+                  </Item>
+                  <Item value="Generate text" subtitle="Expand a prompt">
+                    <Icon icon="noto:memo" />
+                  </Item>
+                </Command.Group>
+                <Command.Group heading="Operations">
+                  <Item
+                    value="Classify"
+                    subtitle="Group, identify, or categorize"
+                  >
+                    <Icon icon="noto:bullseye" />
+                  </Item>
+                </Command.Group>
+                <Command.Group heading="Source data">
+                  <Item value="Images" subtitle="Use existing images">
+                    <Icon icon="noto:framed-picture" />
+                  </Item>
+                  <Item value="Text" subtitle="Enter text or select files">
+                    <Icon icon="noto:memo" />
+                  </Item>
+                  <Item value="Movies" subtitle="Select movie files or URLs">
+                    <Icon icon="noto:movie-camera" />
+                  </Item>
+                </Command.Group>
+              </div>
+              <hr cmdk-framer-separator="" />
+              <div cmdk-framer-right="">
+                {value === 'generate image' && <ImageGeneratorBlock />}
+                {value === 'input' && <Input />}
+                {value === 'badge' && <Badge />}
+                {value === 'radio' && <Radio />}
+                {value === 'slider' && <Slider />}
+                {value === 'container' && <Container />}
+              </div>
+            </div>
+          </Command.List>
+        </Command>
+      </div>
+    </Command.Dialog>
   );
 }
 
