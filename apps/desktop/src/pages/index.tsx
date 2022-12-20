8467d05df8761e3493f1fc786443f3724129c530
Matt Buck <matt@voxable.io>
2022-12-20 16:16:37 -0500

    Subscribe to node changes in Tauri webview

--- apps/desktop/src/pages/index.tsx
+++ apps/desktop/src/pages/index.tsx
@@ -1,11 +1,25 @@
-import { useState, createRef } from 'react';
+import { useState, createRef, useEffect, useRef } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
 
 import { Nav, Playfield, CommandBar } from '../../../../libs/ui/src/index';
+import useAppStore, { AppState } from '../../../../libs/ui/src/store';
 
 function App() {
   const [greetMsg, setGreetMsg] = useState('');
   const [name, setName] = useState('');
+
+  const nodesRef = useRef(useAppStore.getState().nodes);
+
+  // Connect to the store on mount, disconnect on unmount, catch state-changes in a reference
+  useEffect(
+    () =>
+      // Update any changed nodes in the database.
+      useAppStore.subscribe((state) => {
+        console.log('new state', state);
+        nodesRef.current = state.nodes;
+      }),
+    []
+  );
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
